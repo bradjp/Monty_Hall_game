5d57b396c04a5fcdc4e2fa8b21056a0ee15343d2
Brad Phillips <bjrp@protonmail.com>
2020-03-28 21:00:26 +0000

    Implements box selection method

--- lib/game.rb
+++ lib/game.rb
@@ -1,13 +1,21 @@
 class Game
 
-  attr_reader :boxes
+  attr_reader :boxes, :chosen_box
 
   def initialize
     @boxes = ['Goat', 'Car', 'Goat']
+    @chosen_box = nil
   end
 
   def shuffle_boxes
-    @boxes.shuffle
+    @boxes.shuffle!
+    'Shuffled!'
+  end
+
+  def select_box(box)
+    return 'Please choose 1,2 or 3' if box > 3 || box < 1
+    @chosen_box = @boxes[box-1]
+    "You have chosen box #{box}"
   end
 
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -9,8 +9,21 @@ describe Game do
   end
   describe '#shuffle_boxes' do
     it 'randomly arranges the array' do
-      expect(subject.boxes).to receive(:shuffle)
-      subject.shuffle_boxes()
+      expect(subject.boxes).to receive(:shuffle!)
+      subject.shuffle_boxes
+    end
+  end
+  describe '#select_box' do
+    it 'allows the player to choose box 1,2 or 3' do
+      subject.select_box(1)
+      expect(subject.chosen_box).to eq(subject.boxes[0])
+      expect(subject.select_box(1)).to eq('You have chosen box 1')
+    end
+    it 'does not allow the player to choose a box other than 1,2 or 3' do
+      current_chosen_box = subject.chosen_box
+      subject.select_box(4)
+      expect(subject.chosen_box).to eq(current_chosen_box)
+      expect(subject.select_box(4)).to eq('Please choose 1,2 or 3')
     end
   end
 end
