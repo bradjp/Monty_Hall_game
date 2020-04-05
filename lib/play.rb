7a20786f42d67c531585dd66fd2126d46f982155
Brad Phillips <bjrp@protonmail.com>
2020-04-05 21:21:50 +0100

    Progresses game method to show_the_goat

--- lib/play.rb
+++ lib/play.rb
@@ -1,5 +1,20 @@
 require_relative 'game'
 
 def play
+  game = Game.new
 
+  puts 'Shuffling boxes...'
+  sleep(2)
+  game.shuffle_boxes
+  puts 'Shuffled!'
+
+  puts 'Select box 1,2 or 3'
+
+  box = gets.chomp.to_i
+
+  game.select_box(box)
+
+  puts "You have chosen box #{box}"
+
+  game.show_the_goat
 end
